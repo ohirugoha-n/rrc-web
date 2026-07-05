7ad0294ecf544ea25463732f48b1f095a2e4813e
ohirugoha-n <yuuki_yk@icloud.com>
2026-07-05 15:50:08 +0900

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const host = req.headers.get('host') ?? '';
+  const pathname = req.nextUrl.pathname;
+
+  const url = req.nextUrl.clone();
+
+  if (host.startsWith('rrcsangi.')) {
+    url.pathname = `/root${pathname}`;
+    return NextResponse.rewrite(url);
+  }
+  if (host.startsWith('sub.rrcsangi.')) {
+    url.pathname = `/sub${pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
