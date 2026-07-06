e2d18d551fec52c348b3fa4cec88dab103e87af3
ohirugoha-n <yuuki_yk@icloud.com>
2026-07-06 20:20:01 +0900

    subdomainから自動でディレクトリ判断

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,12 +3,11 @@ import { type NextRequest, NextResponse } from 'next/server';
 export function proxy(req: NextRequest) {
   const host = req.headers.get('host') ?? '';
   const pathname = req.nextUrl.pathname;
+  const url = req.nextUrl.clone();
 
   if (pathname === '/favicon.ico' || pathname.startsWith('/_next')) {
     return NextResponse.next();
   }
-
-  const url = req.nextUrl.clone();
 
   // localhostで開発中のページに飛ばせるようにすると便利(ここではroot)
   if (host.startsWith('localhost')) {
@@ -16,14 +15,12 @@ export function proxy(req: NextRequest) {
     return NextResponse.rewrite(url);
   }
 
-  if (host.startsWith('rrcsangi.')) {
+  const subdomain = host.split('.')[0];
+  if (subdomain === 'rrcsangi') {
     url.pathname = `/root${pathname}`;
     return NextResponse.rewrite(url);
   }
-  if (host.startsWith('sub.rrcsangi.')) {
-    url.pathname = `/sub${pathname}`;
-    return NextResponse.rewrite(url);
-  }
 
-  return NextResponse.next();
+  url.pathname = `/${subdomain}${pathname}`;
+  return NextResponse.rewrite(url);
 }
