d512254808652087971d1d1a9ebd31dde93ccf40
ohirugoha-n <yuuki_yk@icloud.com>
2026-07-24 16:01:14 +0900

    button demo

--- src/app/root/page.tsx
+++ src/app/root/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { Button } from '@/components/ui/button';
+
 const Page = () => {
   return (
     <div>
@@ -10,6 +12,7 @@ const Page = () => {
         <div className='h-10 w-10 bg-sky-900'>cc</div>
       </div>
       <Image alt='img' height='100' src='/root/img.webp' width='100' />
+      <Button className='h-40 w-100 rounded-md text-2xl'>ボタン</Button>
     </div>
   );
 };
