f6fe355656bfac8c6b963ede6cd53ac4aae3a2ba
ohirugohan <yuuki_yk@icloud.com>
2026-07-27 16:23:33 +0900

    root ヘッダメニュー追加 (#8)
    * header
    
    * 遷移にuseRouterを使う
    
    * チーム、ブログのページを作る
    
    * ボタン透明
    
    * フォントが適用されるように
    
    * アイコンボタン修正
    
    * router つかわないのでけす
    
    * 小さいとき、ホーム消す

---
+++ src/app/root/teams/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p className='bg-lime-300 text-2xl'>teams page</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/root/header.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Link from 'next/link';
+
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+export const Header = () => {
+  const menu_items: { name: string; href: string }[] = [
+    { name: 'ホーム', href: '/' },
+    { name: 'チーム', href: '/teams' },
+    { name: 'ブログ', href: '/blogs' },
+  ];
+  return (
+    <header className='space-between sticky top-0 flex h-20 w-screen flex-row items-center justify-between bg-slate-700 px-2'>
+      <Link
+        className='flex h-16 w-16 flex-col items-center justify-center rounded-3xl bg-white pb-3 leading-1'
+        href='/'
+      >
+        <p className='font-bold text-xl'>RRC</p>
+        <p className='font-bold text-md'>TMCIT</p>
+      </Link>
+      <div className='flex flex-row space-x-4 pr-4'>
+        {menu_items.map((item) => (
+          <Link
+            className={cn(
+              buttonVariants(),
+              item.name === 'ホーム' && 'hidden sm:inline-flex',
+              'bg-transparent',
+              'h-12 rounded-md hover:scale-105 hover:bg-slate-500',
+              'font-bold text-lg text-white',
+            )}
+            href={item.href}
+            key={item.name}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </header>
+  );
+};
